1726e3df3797e7293aad205364447f80ec5bac61
HP <HP@MorningStar>
2024-12-05 09:07:47 +0100

    Création du dépot

---
+++ src/main/java/edu/projetbanking/bankingsystem/BankingSystem.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.projetbanking.bankingsystem;
+
+import static java.lang.System.out;
+
+/**
+ *
+ * @author HP
+ */
+public class BankingSystem {
+
+    public static void main(String[] args) {
+        out.println("Hello World!");
+    }
+}
